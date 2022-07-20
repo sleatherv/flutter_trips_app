ccd9530863c5d32e0ba45bdd0e230875eeb4e407
sleatherv <sleatherv28@hotmail.com>
2022-07-20 18:39:29 -0500

    Firebase auth with google

---
+++ lib/User/repository/firebase_auth_api.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class FireBaseAuthAPI {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  Future<UserCredential> signIn() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+    // Obtain the auth details from the request
+    GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await _auth.signInWithCredential(credential);
+  }
+}
